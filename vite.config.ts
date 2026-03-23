6671967216c9a1196080e1720a0395c27beb9ecc
bluemistmark <bluemistmark@gmail.com>
2026-03-24 00:51:38 +0900

    feat: deploy 관련 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "https://github.com/bluemistmark/Trace.git",
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      "@": resolve(__dirname, "src"),
     },
   },
-})
+});
